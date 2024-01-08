478882d849b4208a0bd738df88cf7eb1af3f4789
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-01-08 14:46:21 -0300

    add icons

--- app/components/stack.tsx
+++ app/components/stack.tsx
@@ -6,7 +6,7 @@ export const Stack: React.FC = () => {
 
   const listIcons = () => {
     return Object.entries(icons).map(([key, value]) => (
-      <img key={key} src={value.src} alt={key} width={60} height={60} title={key}/>
+      <img key={key} src={value.src} alt={key} width={60} height={60} title={key} className="mt-9"/>
     ))
   }
     return (
@@ -14,7 +14,7 @@ export const Stack: React.FC = () => {
           <h2 className="text-sm text-zinc-500">
             Tech Stack
           </h2>
-          <div className="grid grid-cols-4 mt-6 justify-items-center lg:mx-[8rem]">
+          <div className="grid grid-cols-4 justify-items-center lg:mx-[6rem]">
             {listIcons()}
           </div>
         </div>
--- public/StackIcons/Export.tsx
+++ public/StackIcons/Export.tsx
@@ -2,5 +2,10 @@ import js from "./js.png"
 import java from "./java.png"
 import node from "./node-js.png"
 import python from "./python.png"
+import mysql from "./mysql.png"
+import mongodb from "./mongodb.png"
+import spring from "./spring.png"
+import git from "./git.png"
 
-export const icons = {"js": js, "java": java, "node": node, "python": python}
+export const icons = {"js": js, "java": java, "node": node, "python": python,
+ "mysql": mysql, "mongodb": mongodb, "spring": spring, "git": git}
