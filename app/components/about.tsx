38a66ce9ed85c2ba051143e667d42a59ac2bb557
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-01-09 13:22:58 -0300

    add pov desc and fix stack style

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -2,10 +2,19 @@
 import { Stack } from "./stack";
 
 export const AboutMe: React.FC = () => {
+  const about = `I am a passionate full-stack 
+  developer with a strong focus on backend development, 
+  dedicated to my ongoing professional growth. My love for coding 
+  is complemented by a fervent desire to make a significant contribution 
+  to the success of every project I engage with. My goal is to create a lasting impact, 
+  delivering robust and efficient solutions that drive business achievements. 
+  I am always eager to learn and enhance my skills, committed to continuous development,
+   and delivering exceptional results in the world of software development.`
+
     return (
         <div className="my-16 text-center animate-fade-in mx-[2rem] lg:mx-[8rem]">
           <h2 className="text-sm text-zinc-500 ">
-            I am a passionate full-stack developer with a strong focus on backend development, dedicated to my ongoing professional growth. My love for coding is complemented by a fervent desire to make a significant contribution to the success of every project I engage with
+            {about}
           </h2>
           <Stack />
         </div>
--- app/components/stack.tsx
+++ app/components/stack.tsx
@@ -6,15 +6,18 @@ export const Stack: React.FC = () => {
 
   const listIcons = () => {
     return Object.entries(icons).map(([key, value]) => (
-      <img key={key} src={value.src} alt={key} width={60} height={60} title={key} className="mt-9"/>
+      <div className="icon-container mt-6">
+        <img key={key} src={value.src} alt={key} width={60} height={60} title={key} className="icon-image"/>
+        <p className="text-sm text-zinc-500 mt-1">{key}</p>
+      </div>
     ))
   }
     return (
-        <div className="my-16 animate-fade-in mx-[2rem] lg:mx-[8rem]">
+        <div className="my-16 animate-fade-in mx-[2rem] lg:mx-[4rem]">
           <h2 className="text-sm text-zinc-500">
             Tech Stack
           </h2>
-          <div className="grid grid-cols-4 justify-items-center lg:mx-[6rem]">
+          <div className="grid md:grid-cols-4 grid-cols-2 justify-items-center lg:mx-[6rem]">
             {listIcons()}
           </div>
         </div>
