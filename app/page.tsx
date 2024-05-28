a618e4979b06c05eceb7d044add9805c1dbef5d6
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-05-28 09:39:20 -0300

    comment bot message

--- app/page.tsx
+++ app/page.tsx
@@ -44,9 +44,9 @@ export default function Home() {
 
         <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
         <AboutMe />
-        <div className="animate-fade-in z-10">
+{/*         <div className="animate-fade-in z-10">
           <ChatMenu />
-        </div>
+        </div> */}
       </div>
     </div>
   );
